2333d0e898d54b35e40a3aba8d6cae8df3b2c857
Griffin Berlstein <griffred20@gmail.com>
2020-01-25 09:27:00 -0500

    sketched rebuild script

---
+++ scripts/python_webhook/webhook.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import hmac
+
+WEBHOOK_SECRET = os.getenv('WEBHOOK_SECRET')
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+
+    signature = request.headers.get('X-Hub-Signature')
+    body = request.get_json()
+
+    computed_sig = hmac.digest(WEBHOOK_SECRET, body, 'sha1').hexdigest()
+
+    if hmac.compare_digest(computed_sig, signature):
+        print('success')
+    else:
+        print('error')
+
+
+if __name__ == '__main__':
+    app.run()
