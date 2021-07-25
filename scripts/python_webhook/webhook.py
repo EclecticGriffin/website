c8d085b5bf9b014c58c1b2b08da8fc66d76c4e22
Griffin Berlstein <griffin@berlste.in>
2021-07-24 21:39:17 -0400

    update webhook

--- scripts/python_webhook/webhook.py
+++ scripts/python_webhook/webhook.py
@@ -3,26 +3,28 @@ import hmac
 import os
 import subprocess
 
-WEBHOOK_SECRET = bytes(os.getenv('WEBHOOK_SECRET'), 'utf-8')
+WEBHOOK_SECRET = bytes(os.getenv("WEBHOOK_SECRET"), "utf-8")
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
+
+@app.route("/webhook", methods=["POST"])
 def webhook():
 
-    signature = request.headers.get('X-Hub-Signature', '')
+    signature = request.headers.get("X-Hub-Signature", "")
     body = request.get_data()
 
-    computed_sig = 'sha1=' + hmac.HMAC(WEBHOOK_SECRET, body, 'sha1').hexdigest()
+    computed_sig = "sha1=" + hmac.HMAC(WEBHOOK_SECRET, body, "sha1").hexdigest()
 
     if hmac.compare_digest(computed_sig, signature):
-        print('success')
+        print("success")
         subprocess.run(["git", "pull"])
-        subprocess.run(["hugo"])
+        subprocess.run(["zola", "build"])
     else:
-        print('error')
+        print("error")
 
     return "200"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
